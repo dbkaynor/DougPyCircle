bd768d740c86438d353a087ddfb62590fefd3964
Douglas B Kaynor <dbkaynor@gmail.com>
2022-03-15 12:36:59 -0700

    cleanup the folder

--- keywords.py
+++
@@ -1,9 +0,0 @@
-
-# Python code to demonstrate working of iskeyword()
-
-# importing "keyword" for keyword operations
-import keyword
-
-# printing all keywords at once using "kwlist()"
-print("The list of keywords is : ")
-print(keyword.kwlist)
